50215c74e3a30f8d16589c85719267809f8782dd
kuki <ubarcturus@gmail.com>
2022-10-24 13:51:07 +0200

    add ordinal numbers and gifts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let ordinal_numbers = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+    let gifts_mirth_without_mischief = [
+        "a Partridge in a pear-tree",
+        "two Turtle doves, and",
+        "three French hens,",
+        "four Colly birds,",
+        "five Gold rings,",
+        "six Geese a laying,",
+        "seven Swans a swimming,",
+        "eight Maids a milking,",
+        "nine Drummers drumming,",
+        "ten Pipers piping,",
+        "eleven Ladies dancing,",
+        "twelve Lords a leaping,",
+    ];
 }
